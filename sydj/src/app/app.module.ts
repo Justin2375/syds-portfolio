3a8b37d9234009e861a0ecfcf373769e683958aa
Justin2375 <justin.jarrard@siu.edu>
2018-05-31 10:38:08 -0400

    set up basic pages and configured routing

--- sydj/src/app/app.module.ts
+++ sydj/src/app/app.module.ts
@@ -2,13 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArtworkComponent } from './artwork/artwork.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: 'artwork', component: ArtworkComponent
+  },
+  {
+    path: 'contact', component: ContactComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtworkComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+    //...
   ],
   providers: [],
   bootstrap: [AppComponent]
